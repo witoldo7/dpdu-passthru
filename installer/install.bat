2418b3b6f88a6a7904af96d7ff0bb55b25cc4490
JohnJocke <johnjocke@asd.com>
2023-02-09 11:50:02 +0200

    Fix installer bat

--- installer/install.bat
+++ installer/install.bat
@@ -2,24 +2,24 @@
 
 pushd %~dp0
 
-if exist "C:\Program Files (x86)\macchina\dpdpu" echo Driver folder already exists, ignoring
-if not exist "C:\Program Files (x86)\macchina\dpdu" mkdir "C:\Program Files (x86)\macchina\dpdu"
+if exist "C:\Program Files (x86)\dpdu_j2534" echo Driver folder already exists, ignoring
+if not exist "C:\Program Files (x86)\dpdu_j2534" mkdir "C:\Program Files (x86)\dpdu_j2534"
 
-if not exist "C:\ProgramData\D-PDU API" (
+if not exist "C:\Program Files (x86)\D-PDU API" (
 	echo Error. No D-PDU API Folder found!
 	pause
 	exit
 )
 
 echo "Copying files"
-xcopy /s/y "pdu_api_root.xml" "C:\ProgramData\D-PDU API"
+xcopy /s/y "pdu_api_root.xml" "C:\Program Files (x86)\D-PDU API"
 if errorlevel 1 (
 	echo Error copying DPDPU XML file
 	pause
 	exit
 )
 
-xcopy /s/y "*_macchina_m2.xml" "C:\Program Files (x86)\macchina\dpdu"
+xcopy /s/y "..\driver\Release\driver.dll" "C:\Program Files (x86)\dpdu_j2534"
 if errorlevel 1 (
 	echo Error copying DPDPU CDF/MDF XML file
 	pause
